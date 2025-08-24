cfca268f32db64afc4ddd3dbd415ff90f48bd98a
OliverDong <oliver.dong@oocl.com>
2025-08-24 10:28:12 +0800

    DEV: realize "M" part of execute method and report method

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -9,7 +9,19 @@ public class MarsRover {
     }
     int getX(){return this.x;}
     int getY(){return this.y;}
-    int getDirection(){return this.direction.ordinal();}
+    String getDirection(){return this.direction.name();}
 
 
+    public void execute(String command) {
+        if(command.equals("M")){
+            if(this.direction==DIRECTION.S) this.y--;
+            else if(this.direction==DIRECTION.N) this.y++;
+            else if(this.direction==DIRECTION.E) this.x++;
+            else this.x--;
+        }
+    }
+
+    public String report() {
+        return String.format("(%d,%d) %s",x,y,direction, direction.name());
+    }
 }
